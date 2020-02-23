c9614e3ff1d87ca96d994fdbf3e437869c213570
Vaibhav Arora <varora@catalystone.com>
2020-02-23 22:13:19 +0530

    Implemented Selection sort

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var search_algo = document.getElementById('search_algo');
 var searchInput = document.getElementById('searchInput');
 
 var searchResult = document.getElementById('search_result');
+var sortResult = document.getElementById('sort_result');
 var timeTakenElement = document.getElementById('time_taken');
 
 var randomArray = [];
@@ -37,7 +38,7 @@ function updateArrayInDOM(array) {
     arrayDisplay.innerHTML = template;
 }
 
-function startSearch() {
+function start() {
     clearResults();
 
     switch (Number(search_algo.selectedOptions[0].id)) {
@@ -48,7 +49,7 @@ function startSearch() {
             binarySearch();
             break;
         case 3:
-            // perform Selection sort
+            selectionSort();
             break;
         case 4:
             // perform bubble sort
@@ -68,11 +69,11 @@ async function linearSearch() {
 
         if (ele === Number(searchInput.value)) {
             elementInDOM.classList.add('box-scale', 'success');
-            displayResults(performance.now() - startTimer, Number(searchInput.value), true);
+            displaySearchResults(performance.now() - startTimer, Number(searchInput.value), true);
             break;
         } else {
             if (i === (randomArray.length - 1)) {
-                displayResults(performance.now() - startTimer, Number(searchInput.value), false);
+                displaySearchResults(performance.now() - startTimer, Number(searchInput.value), false);
             } else {
                 elementInDOM.classList.add('box-scale', 'error');
             }
@@ -84,7 +85,7 @@ async function linearSearch() {
 async function binarySearch() {
     toggleSearchingText(true);
     let startTimer = performance.now();
-    let resultFound = false;;
+    let resultFound = false;
 
     randomArray.sort((a, b) => a - b);
     updateArrayInDOM(randomArray);
@@ -95,7 +96,7 @@ async function binarySearch() {
 
     if(endIndex - startIndex === 1) {
         resultFound = true;
-        displayResults(performance.now() - startTimer, Number(searchInput.value), false);
+        displaySearchResults(performance.now() - startTimer, Number(searchInput.value), false);
     }
 
     while(!resultFound) {
@@ -115,7 +116,7 @@ async function binarySearch() {
         
         if(ele === Number(searchInput.value)) {
             elementInDOM.classList.add('box-scale', 'success');
-            displayResults(performance.now() - startTimer, Number(searchInput.value), true);
+            displaySearchResults(performance.now() - startTimer, Number(searchInput.value), true);
             resultFound = true;
         } else {
             elementInDOM.classList.add('box-scale', 'error');
@@ -123,13 +124,51 @@ async function binarySearch() {
 
         if(endIndex - startIndex === 1) {
             resultFound = true;
-            displayResults(performance.now() - startTimer, Number(searchInput.value), false);
+            displaySearchResults(performance.now() - startTimer, Number(searchInput.value), false);
         }
 
         await wait();
     }
 }
 
+async function selectionSort() {
+    toggleSortingText(true);
+    let startTimer = performance.now();
+    for (let i = 0; i < randomArray.length; i++) {
+        let ele = randomArray[i];
+        let smallestNumber = { 'position': i, 'number': ele};
+
+        document.querySelectorAll('.box-scale').forEach((ele) => {
+            ele.classList.remove('box-scale', 'error', 'success');
+        });
+
+        document.getElementById(`box_${ele}`).classList.add('box-scale', 'error');
+        
+        for(let j = i + 1; j < randomArray.length; j++) {
+            let ele_1 = randomArray[j];
+            document.getElementById(`box_${ele_1}`).classList.add('box-scale' ,'error');
+            
+            if(smallestNumber.number > ele_1) {
+                smallestNumber.number = ele_1;
+                smallestNumber.position = j;
+            }
+            await wait();
+            document.getElementById(`box_${ele_1}`).classList.remove('box-scale' ,'error');
+        }
+
+        if(smallestNumber.number !== ele) {
+            document.getElementById(`box_${ele}`).classList.remove('error');
+            document.getElementById(`box_${ele}`).classList.add('success');
+            document.getElementById(`box_${randomArray[smallestNumber.position]}`)
+                .classList.add('box-scale', 'success');
+            randomArray[i] = smallestNumber.number;
+            randomArray[smallestNumber.position] = ele;
+            updateArrayInDOM(randomArray);
+        }
+        await wait();
+    }
+    displaySortingResults(performance.now() - startTimer);
+}
 
 async function wait() {
     return new Promise((resolve, reject) => {
@@ -139,7 +178,7 @@ async function wait() {
     });
 }
 
-function displayResults(timeTaken, searchEle, isSearchSuccessful) {
+function displaySearchResults(timeTaken, searchEle, isSearchSuccessful, sortingSuccessful) {
     toggleSearchingText(false);
     searchResult.innerHTML = `Found ${searchEle}`;
 
@@ -150,8 +189,15 @@ function displayResults(timeTaken, searchEle, isSearchSuccessful) {
     timeTakenElement.innerHTML = `Time taken ${(timeTaken / 1000).toFixed(2)} seconds`;
 }
 
+function displaySortingResults(timeTaken) {
+    toggleSortingText(false);
+    sortResult.innerHTML = `Sorting is done`;
+    timeTakenElement.innerHTML = `Time taken ${(timeTaken / 1000).toFixed(2)} seconds`;
+}
+
 function clearResults() {
     searchResult.innerHTML = '';
+    sortResult.innerHTML = '';
     timeTakenElement.innerHTML = '';
 }
 
@@ -160,5 +206,21 @@ function toggleSearchingText(show) {
         document.getElementById('searching').classList.remove('hidden');
     } else {
         document.getElementById('searching').classList.add('hidden');
+    }
+}
+
+function toggleSortingText(show) {
+    if (show) {
+        document.getElementById('sorting').classList.remove('hidden');
+    } else {
+        document.getElementById('sorting').classList.add('hidden');
+    }
+}
+
+function refreshOptions(id) {
+    if(Number(id) === 3 || Number(id) === 4) {
+        searchInput.disabled = true;
+    } else {
+        searchInput.disabled = false;
     }
 }
